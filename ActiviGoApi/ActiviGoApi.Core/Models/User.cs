bec6a384275575d10e75f0559ddf258683d3e081
darkdusk234 <johan.svensson.01@hotmail.com>
2025-09-30 11:08:04 +0200

    Created model for User

---
+++ ActiviGoApi/ActiviGoApi.Core/Models/User.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+
+namespace ActiviGoApi.Core.Models
+{
+    internal class User : IdentityUser
+    {
+        [Required]
+        [MinLength(2)]
+        public string FirstName { get; set; } = string.Empty;
+        [Required]
+        [MinLength(2)]
+        public string LastName { get; set; } = string.Empty;
+        [Required]
+        public DateTime DateOfBirth { get; set; }
+        public string? Address { get; set; }
+        public bool IsSuspended { get; set; } = false;
+    }
+}
