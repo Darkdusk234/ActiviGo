d09e561446f5b814ee437263dde60ef11351167f
GoodStuff15 <gustav.emil.eriksson@gmail.com>
2025-09-30 14:59:23 +0200

    Created genric repo and location repo (example)

---
+++ ActiviGoApi/ActiviGoApi.Core/Interfaces/ILocationRepository.cs
@@ -0,0 +1,13 @@
+﻿using ActiviGoApi.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ActiviGoApi.Core.Interfaces
+{
+    public interface ILocationRepository : IGenericRepository<Location>
+    {
+    }
+}
---
+++ ActiviGoApi/ActiviGoApi.Infrastructur/Repositories/GenericRepository.cs
@@ -0,0 +1,58 @@
+﻿using ActiviGoApi.Core.Interfaces;
+using ActiviGoApi.Infrastructur.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Conventions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace ActiviGoApi.Infrastructur.Repositories
+{
+    public class GenericRepository<TEntity> : IGenericRepository<TEntity> where TEntity : class
+    {
+        private readonly DbSet<TEntity> _dbSet;
+
+        public GenericRepository(ToadContext context)
+        {
+            _dbSet = context.Set<TEntity>();
+        }
+
+        public async Task<TEntity> AddAsync(TEntity entity)
+        {
+            await _dbSet.AddAsync(entity);
+            return entity;
+        }
+
+        public async Task<bool> DeleteAsync(int id)
+        {
+            var entity = await _dbSet.FindAsync(id);
+
+            if (entity == null)
+            {
+                return false;
+            }
+
+            _dbSet.Remove(entity);
+            return true;
+        }
+
+        public async Task<IEnumerable<TEntity>> GetAllAsync()
+        {
+            return await _dbSet.ToListAsync();
+        }
+
+        public async Task<TEntity> GetByIdAsync(int id)
+        {
+            return await _dbSet.FindAsync(id);
+        }
+
+        public async Task<TEntity> UpdateAsync(TEntity entity)
+        {
+            _dbSet.Update(entity);
+            return entity;
+        }
+    }
+}
