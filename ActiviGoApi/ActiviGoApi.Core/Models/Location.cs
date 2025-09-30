34071ee6132586a4d76ff5b1f62a192c1f9d6489
darkdusk234 <johan.svensson.01@hotmail.com>
2025-09-30 11:19:33 +0200

    Created model for Location

---
+++ ActiviGoApi/ActiviGoApi.Core/Models/Location.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ActiviGoApi.Core.Models
+{
+    public class Location
+    {
+        [Required]
+        public int Id { get; set; }
+        [Required]
+        [MinLength(3)]
+        public string Name { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public string Adress { get; set; } = string.Empty;
+        [Required]
+        public string Latitude { get; set; } = string.Empty;
+        [Required]
+        public string Longitude { get; set; } = string.Empty;
+        public int? Capacity { get; set; }
+        public bool IsIndoors { get; set; } = true;
+        public bool IsActive { get; set; } = true;
+
+        //Navigation Properties
+        public ICollection<Activity>? Activities { get; set; }
+        public ICollection<ActivityOccurence>? ActivitOccurrences { get; set; }
+    }
+}
