1becf328fb4903d4587bdbb482380b9d16b4925d
GoodStuff15 <gustav.emil.eriksson@gmail.com>
2025-10-01 09:21:34 +0200

    Added a unit of work pattern interface and class, with example (location repository)

---
+++ ActiviGoApi/ActiviGoApi.Infrastructur/Repositories/IUnitOfWork.cs
@@ -0,0 +1,10 @@
+﻿namespace ActiviGoApi.Infrastructur.Repositories
+{
+    public interface IUnitOfWork
+    {
+        // Add properties for each repository with only a getter
+        LocationRepository Locations { get; }
+
+        Task<bool> SaveChangesAsync(CancellationToken ct = default);
+    }
+}
