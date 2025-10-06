36824829fb5378e03a596a736d72845340acdf10
Leon <leon.json57@gmail.com>
2025-10-06 22:37:10 +0200

    ilocal

--- ActiviGoApi/ActiviGoApi.Services/Interfaces/ILocationService.cs
+++ ActiviGoApi/ActiviGoApi.Services/Interfaces/ILocationService.cs
@@ -1,6 +1,13 @@
-﻿namespace ActiviGoApi.Services.Interfaces
+﻿using ActiviGoApi.Core.Models;
+
+namespace ActiviGoApi.Services.Interfaces
 {
     public interface ILocationService
     {
+        Task<IEnumerable<Location>>GetAllAsync(CancellationToken ct = default);
+        Task<Location?> GetByIdAsync(int id, CancellationToken ct = default);
+        Task <Location>AddAsync(CancellationToken ct = default);    // CreateAsync
+        Task<Location?> UpdateAsync(int id, CancellationToken ct = default);
+        Task<bool> DeleteAsync(int id, CancellationToken ct = default);
     }
 }
