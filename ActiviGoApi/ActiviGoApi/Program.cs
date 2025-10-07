af2cae2774f75f7aa46c8bb6c130bce7a6b7da8b
Edgarskonnegard <j.edgarskonnegard@gmail.com>
2025-10-07 10:08:54 +0200

    added scope for booking service.

--- ActiviGoApi/ActiviGoApi/Program.cs
+++ ActiviGoApi/ActiviGoApi/Program.cs
@@ -1,4 +1,6 @@
 
+using ActiviGoApi.Services;
+using ActiviGoApi.Services.Interfaces;
 using ActiviGoApi.Services.Mapping;
 using ActiviGoApi.Services.Validation.DataValidation;
 using FluentValidation;
@@ -12,6 +14,8 @@ namespace ActiviGoApi
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
+            builder.Services.AddScoped<IBookingService, BookingService>();
+
 
             // Adding FluentValidation
             builder.Services.AddValidatorsFromAssemblyContaining<LocationRequestDTO_Validator>();
