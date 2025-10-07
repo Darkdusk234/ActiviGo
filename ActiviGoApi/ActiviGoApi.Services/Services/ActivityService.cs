0156786773c4c4c296e1f351a0c326d01438d151
GoodStuff15 <gustav.emil.eriksson@gmail.com>
2025-10-07 09:54:05 +0200

    Fixed project references that were faulty

---
+++ ActiviGoApi/ActiviGoApi.Services/Services/ActivityService.cs
@@ -0,0 +1,18 @@
+﻿using ActiviGoApi.Infrastructur.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ActiviGoApi.Services.Services
+{
+    public class ActivityService
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        public ActivityService(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+    }
+}
