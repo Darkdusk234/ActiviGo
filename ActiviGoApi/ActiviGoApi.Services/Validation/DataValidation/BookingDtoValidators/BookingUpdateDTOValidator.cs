239ea284e87b1b1695a437a4ba5b1f85a6b93bf2
Edgarskonnegard <j.edgarskonnegard@gmail.com>
2025-10-07 10:41:07 +0200

    added more rules for validator

--- ActiviGoApi/ActiviGoApi.Services/Validation/DataValidation/BookingDtoValidators/BookingUpdateDTOValidator.cs
+++ ActiviGoApi/ActiviGoApi.Services/Validation/DataValidation/BookingDtoValidators/BookingUpdateDTOValidator.cs
@@ -12,5 +12,10 @@ public class BookingUpdateDTOValidator : AbstractValidator<BookingUpdateDTO>
         RuleFor(x => x)
             .Must(dto => !(dto.IsCancelled && dto.ParticipationConfirmed))
             .WithMessage("A booking cannot be both cancelled and confirmed.");
+        // Om man ska updatera en bokning till betald måste den också vara satt till inaktiv
+        RuleFor(x => x)
+            .Must(dto => !(dto.IsPaid && !dto.IsActive))
+            .WithMessage("A paid booking cannot be marked as inactive.");
+
     }
 }
