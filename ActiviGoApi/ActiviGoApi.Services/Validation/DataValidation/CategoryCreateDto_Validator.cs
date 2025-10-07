f80fb3dd8158b40294fae216a6fa64e26e1553e8
darkdusk234 <johan.svensson.01@hotmail.com>
2025-10-07 11:23:35 +0200

    Created validator for CategoryCreateDto

---
+++ ActiviGoApi/ActiviGoApi.Services/Validation/DataValidation/CategoryCreateDto_Validator.cs
@@ -0,0 +1,18 @@
+﻿using ActiviGoApi.Services.DTOs.CategpryDtos;
+using FluentValidation;
+
+namespace ActiviGoApi.Services.Validation.DataValidation
+{
+    public class CategoryCreateDto_Validator : AbstractValidator<CategoryCreateDto>
+    {
+        public CategoryCreateDto_Validator()
+        {
+            RuleFor(c => c.Name)
+                .NotEmpty().WithMessage("Name is required.")
+                .MaximumLength(100).WithMessage("Name cannot exceed 100 characters.");
+
+            RuleFor(c => c.Description)
+                .MaximumLength(500).WithMessage("Description cannot exceed 500 characters.");
+        }
+    }
+}
