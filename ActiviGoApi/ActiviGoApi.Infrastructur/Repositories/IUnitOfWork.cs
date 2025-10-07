12d37db5d4a81b058209e830fec401f86c73aac7
darkdusk234 <johan.svensson.01@hotmail.com>
2025-10-07 10:48:46 +0200

    Added generic repository for category

--- ActiviGoApi/ActiviGoApi.Infrastructur/Repositories/IUnitOfWork.cs
+++ ActiviGoApi/ActiviGoApi.Infrastructur/Repositories/IUnitOfWork.cs
@@ -1,9 +1,13 @@
-﻿namespace ActiviGoApi.Infrastructur.Repositories
+﻿using ActiviGoApi.Core.Interfaces;
+using ActiviGoApi.Core.Models;
+
+namespace ActiviGoApi.Infrastructur.Repositories
 {
     public interface IUnitOfWork
     {
         // Add properties for each repository with only a getter
         LocationRepository Locations { get; }
+        IGenericRepository<Category> Categories { get; }
 
         Task<bool> SaveChangesAsync(CancellationToken ct = default);
     }
