4955c2934fbca6266d0c59c4efd196e9fd882cbd
Conixen <leon.json57@gmail.com>
2025-10-07 11:00:16 +0200

    response dto

---
+++ ActiviGoApi/ActiviGoApi.Services/DTOs/ActivityOccurenceDTOs/ActivityOccurenceResponseDTO.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ActiviGoApi.Services.DTOs.ActivityOccurenceDTOs
+{
+    public class ActivityOccurenceResponseDTO
+    {
+        public int Id { get; set; }
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+        public int Capacity { get; set; }
+        public int AvailableSpots { get; set; }
+        public bool IsCancelled { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+
+        // Foreign Keys
+        public int ActivityId { get; set; }
+        public int LocationId { get; set; }
+    }
+}
