30f3272ea8757f563022ae8be7cf3876834fa5de
Conixen <leon.json57@gmail.com>
2025-10-07 11:55:17 +0200

    interface

--- ActiviGoApi/ActiviGoApi.Services/Interfaces/IActivityOccurence.cs
+++ ActiviGoApi/ActiviGoApi.Services/Interfaces/IActivityOccurence.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace ActiviGoApi.Services.Interfaces
 {
-    public class IActivityOccurence
+    public interface IActivityOccurence
     {
         Task<IEnumerable<ActivityOccurence>> GetAllAsync(CancellationToken ct = default);
         Task<ActivityOccurence?> GetByIdAsync(int id, CancellationToken ct = default);
