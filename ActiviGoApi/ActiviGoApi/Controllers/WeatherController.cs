c5475b62512542148ffc6aa7956dc3a1eefb50b8
GoodStuff15 <gustav.emil.eriksson@gmail.com>
2025-10-08 09:47:13 +0200

    Installed RestSharp to help handle 3rd party api calls, created controller

---
+++ ActiviGoApi/ActiviGoApi/Controllers/WeatherController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using RestSharp;
+
+namespace ActiviGoApi.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class WeatherController : ControllerBase
+    {
+        [HttpGet("/LocationAndTime")]
+        public async Task<ActionResult<WeatherResponseDTO>> GetWeatherByLocationAndTime([FromBody] WeatherRequestDTO request)
+        {
+
+        }
+    }
+}
