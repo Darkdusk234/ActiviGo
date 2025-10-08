ded53861973719c02af4b2fe13363aff27d9b016
GoodStuff15 <gustav.emil.eriksson@gmail.com>
2025-10-08 09:52:25 +0200

    Created DTOs for weather calls

---
+++ ActiviGoApi/ActiviGoApi.Services/DTOs/WeatherDTOs/WeatherRequestDTO.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ActiviGoApi.Services.DTOs.WeatherDTOs
+{
+    public record WeatherRequestDTO
+    {
+        //public string Adress { get; init; } = string.Empty; // Probably not necessary?
+        public string? Latitiude { get; init; } = string.Empty;
+        public string? Longitude { get; init; } = string.Empty;
+
+        public DateOnly Date { get; init; }
+        public TimeOnly? Time { get; init; }
+    }
+}
---
+++ ActiviGoApi/ActiviGoApi.Services/DTOs/WeatherDTOs/WeatherResponseDTO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ActiviGoApi.Services.DTOs.WeatherDTOs
+{
+    public record WeatherResponseDTO
+    {
+        public string WeatherDescription { get; init; } = string.Empty;
+        public float TemperatureCelsius { get; init; }
+        public string WeatherImgUrl { get; init; } = string.Empty;
+        public DateTime ObservationTime { get; init; }
+    }
+}
