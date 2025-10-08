1e453f58c061c683e59126639c731fc357b90c35
darkdusk234 <johan.svensson.01@hotmail.com>
2025-10-08 13:55:21 +0200

    Created method to generate JWTToken for users logging in

--- ActiviGoApi/ActiviGoApi/Controllers/AuthController.cs
+++ ActiviGoApi/ActiviGoApi/Controllers/AuthController.cs
@@ -4,6 +4,10 @@ using FluentValidation;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 
 namespace ActiviGoApi.WebApi.Controllers
 {
@@ -49,6 +53,33 @@ namespace ActiviGoApi.WebApi.Controllers
             }
 
             return Ok();
+        }
+
+        private async Task<string> GenerateJwtTokenAsync(User user)
+        {
+            var jwt = _config.GetSection("Jwt");
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwt["Key"]!));
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            var roles = await _users.GetRolesAsync(user);           // <-- fetch roles
+            var roleClaims = roles.Select(r => new Claim(ClaimTypes.Role, r));
+
+            var claims = new List<Claim>
+             {
+                new Claim(JwtRegisteredClaimNames.Sub, user.UserName!),
+                new Claim(ClaimTypes.NameIdentifier, user.Id),
+                new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString())
+            };
+            claims.AddRange(roleClaims);
+
+            var token = new JwtSecurityToken(
+                issuer: jwt["Issuer"],
+                audience: jwt["Audience"],
+                claims: claims,
+                expires: DateTime.UtcNow.AddMinutes(double.Parse(jwt["ExpiresInMinutes"]!)),
+                signingCredentials: creds
+            );
+            return new JwtSecurityTokenHandler().WriteToken(token);
         }
     }
 }
