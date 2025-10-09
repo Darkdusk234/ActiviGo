5f6f6fd06a2bb34538bce6af9a58664666f88a0e
Leon <leon.json57@gmail.com>
2025-10-09 11:07:13 +0200

    request dto

--- ActiviGoApi/ActiviGoApi.Services/DTOs/LocationDTOs/LocationRequestDTO.cs
+++ ActiviGoApi/ActiviGoApi.Services/DTOs/LocationDTOs/LocationRequestDTO.cs
@@ -3,11 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ActiviGoApi.Core.Models;
 
 namespace ActiviGoApi.Services.DTOs
 {
-    public record LocationRequestDTO
+    public class LocationRequestDTO
     {
-        public string Name { get; init; } = string.Empty;
+        public int Id { get; set; }
+
+        public string Name { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public string Adress { get; set; } = string.Empty;
+
+        public string Latitude { get; set; } = string.Empty;
+
+        public string Longitude { get; set; } = string.Empty;
+        public bool IsActive { get; set; } = true;
+
+
     }
 }
