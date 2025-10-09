94171dc1ebf71dbb45d9f27c965ff0e2dbc17ece
Leon <leon.json57@gmail.com>
2025-10-09 23:13:31 +0200

    location => dtos

--- ActiviGoApi/ActiviGoApi.Services/Interfaces/ILocationService.cs
+++ ActiviGoApi/ActiviGoApi.Services/Interfaces/ILocationService.cs
@@ -1,14 +1,15 @@
 ﻿using ActiviGoApi.Core.Models;
+using ActiviGoApi.Services.DTOs;
 using ActiviGoApi.Services.DTOs.LocationDTOs;
 
 namespace ActiviGoApi.Services.Interfaces
 {
     public interface ILocationService
     {
-        Task<IEnumerable<Location>>GetAllAsync(CancellationToken ct = default);
-        Task<Location?> GetByIdAsync(int id, CancellationToken ct = default);
-        Task <Location>AddAsync(CreateLocationDTO createDto, CancellationToken ct = default);    // CreateAsync
-        Task<Location?> UpdateAsync(int id, UpdateLocationDTO updateLocal, CancellationToken ct = default);
+        Task<IEnumerable<LocationRequestDTO>>GetAllAsync(CancellationToken ct = default);
+        Task<LocationRequestDTO?> GetByIdAsync(int id, CancellationToken ct = default);
+        Task <LocationRequestDTO> AddAsync(CreateLocationDTO createDto, CancellationToken ct = default);    // CreateAsync
+        Task<LocationRequestDTO?> UpdateAsync(int id, UpdateLocationDTO updateLocal, CancellationToken ct = default);
         Task DeleteAsync(int id, CancellationToken ct = default);
     }
 }
