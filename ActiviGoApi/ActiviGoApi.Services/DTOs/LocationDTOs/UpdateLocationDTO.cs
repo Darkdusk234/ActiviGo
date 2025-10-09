d0179bcd9653c11ed7833979ee9e2e8d4e5d1420
Leon <leon.json57@gmail.com>
2025-10-09 11:07:05 +0200

    update dto

---
+++ ActiviGoApi/ActiviGoApi.Services/DTOs/LocationDTOs/UpdateLocationDTO.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ActiviGoApi.Services.DTOs.LocationDTOs
+{
+    public class UpdateLocationDTO
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public string Adress { get; set; } = string.Empty;
+        public string Latitude { get; set; } = string.Empty;
+        public string Longitude { get; set; } = string.Empty;
+        public bool IsActive { get; set; } = true;
+        public DateTime UpdatedAt { get; set; } = DateTime.UtcNow;
+
+    }
+}
