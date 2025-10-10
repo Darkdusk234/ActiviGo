4abfe1bc61abec8f7598f4a07114595157fd59e2
GoodStuff15 <gustav.emil.eriksson@gmail.com>
2025-10-10 11:14:36 +0200

    Updated DTOs after model changes

--- ActiviGoApi/ActiviGoApi.Services/DTOs/ActivityDTOs/GetActivityResponse.cs
+++ ActiviGoApi/ActiviGoApi.Services/DTOs/ActivityDTOs/GetActivityResponse.cs
@@ -9,12 +9,13 @@ namespace ActiviGoApi.Services.DTOs.ActivityDTOs
     public record GetActivityResponse
     {
         public string ActivityId { get; set; } = string.Empty;
-        public string Title { get; init; } = string.Empty;
+        public string Name { get; init; } = string.Empty;
         public string Description { get; init; } = string.Empty;
-        public DateTime Date { get; init; }
-        public string Location { get; init; } = string.Empty;
+        public int DurationInMinutes { get; init; }
+        public string IMGUrl { get; init; } = string.Empty;
         public int MaxParticipants { get; init; }
         public decimal Price { get; init; }
+        public int CategoryId { get; init; }    
 
     }
 }
