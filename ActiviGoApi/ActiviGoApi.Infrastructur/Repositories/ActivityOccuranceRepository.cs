25df308630d8c90f25565f8390c3c363f6b6473a
GoodStuff15 <gustav.emil.eriksson@gmail.com>
2025-10-10 11:45:09 +0200

    Created a filtersearch-dto and added filter search to service / repo for activityoccurence

--- ActiviGoApi/ActiviGoApi.Infrastructur/Repositories/ActivityOccuranceRepository.cs
+++ ActiviGoApi/ActiviGoApi.Infrastructur/Repositories/ActivityOccuranceRepository.cs
@@ -1,8 +1,10 @@
 ﻿using ActiviGoApi.Core.Models;
 using ActiviGoApi.Infrastructur.Data;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,9 +12,23 @@ namespace ActiviGoApi.Infrastructur.Repositories
 {
     public class ActivityOccuranceRepository : GenericRepository<ActivityOccurence>
     {
+        private DbSet<ActivityOccurence> _dbSet;
+
         public ActivityOccuranceRepository(ToadContext context) : base(context)
         {
+            _dbSet = context.Set<ActivityOccurence>();
+        }
 
+        public async Task<IEnumerable<ActivityOccurence>> GetFilteredAsync(Expression<Func<ActivityOccurence, bool>> filter = null, CancellationToken ct = default)
+        {
+            IQueryable<ActivityOccurence> query = _dbSet;
+
+            if (filter != null)
+            {
+                query = query.Where(filter);
+            }
+
+            return await query.ToListAsync();
         }
     }
 }
---
+++ ActiviGoApi/ActiviGoApi.Services/DTOs/ActivityOccurenceDTOs/ActivityOccurenceSearchFilterDTO.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ActiviGoApi.Services.DTOs.ActivityOccurenceDTOs
+{
+    public record ActivityOccurenceSearchFilterDTO
+    {
+        public string? NameFilter { get; init; }
+        public int? SubLocationId { get; init; }
+        public int? CategoryId { get; init; }
+        public int? ActivityId { get; init; }
+        public bool? AvailableToBook { get; init; }
+
+        // For geospatial filtering - not implemented
+        //public string? NearLatitude { get; init; }
+        //public string? NearLongitude { get; init; }
+        //public int? WithinKilometers { get; init; }
+
+        public DateTime? StartTime { get; init; }
+
+        public DateTime? EndTime { get; init; }
+    }
+}
