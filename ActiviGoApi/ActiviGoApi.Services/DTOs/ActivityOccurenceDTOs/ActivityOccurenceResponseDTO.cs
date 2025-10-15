fb9e35a5b8e61f7630d0ccd76286f32e56e877f1
Edgarskonnegard <j.edgarskonnegard@gmail.com>
2025-10-15 12:42:54 +0200

    Added parameters to dto.

--- ActiviGoApi/ActiviGoApi.Services/DTOs/ActivityOccurenceDTOs/ActivityOccurenceResponseDTO.cs
+++ ActiviGoApi/ActiviGoApi.Services/DTOs/ActivityOccurenceDTOs/ActivityOccurenceResponseDTO.cs
@@ -9,6 +9,10 @@ namespace ActiviGoApi.Services.DTOs.ActivityOccurenceDTOs
     public class ActivityOccurenceResponseDTO
     {
         public int Id { get; set; }
+        public string ActivityName { get; set; } = string.Empty;
+        public string SubLocationName { get; set; } = string.Empty;
+        public string IMGUrl { get; init; } = string.Empty;
+        public decimal Price { get; init; }
         public DateTime StartTime { get; set; }
         public DateTime EndTime { get; set; }
         public int Capacity { get; set; }
