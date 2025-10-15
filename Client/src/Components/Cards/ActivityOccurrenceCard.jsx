4db703d3d8ecf53f39ff30eceb6f03c519bb4c21
Edgarskonnegard <j.edgarskonnegard@gmail.com>
2025-10-15 12:44:05 +0200

    Added new parameters to component display.

--- Client/src/Components/Cards/ActivityOccurrenceCard.jsx
+++ Client/src/Components/Cards/ActivityOccurrenceCard.jsx
@@ -2,21 +2,31 @@ import "./ActivityOccurrenceCard.css";
 export default function ActivityOccurrenceCard({ occurrence }) {
   return (
     <div className="occurrence-card">
+        <img
+        src={occurrence.IMGUrl|| "https://via.placeholder.com/300x200"}
+        alt={"bild"}
+      />
       <div className="occurrence-card-content">
         <h3 className="occurrence-card-title">
-          Aktivitet #{occurrence.activityId}
+          {occurrence.activityName}
         </h3>
         <p className="occurrence-card-info">
-          🗓 Start: {new Date(occurrence.startTime).toLocaleString("sv-SE")}
+          Datum: {new Date(occurrence.startTime).toLocaleDateString("sv-SE", { day: '2-digit', month: '2-digit' })}
         </p>
         <p className="occurrence-card-info">
-          ⏰ Slut: {new Date(occurrence.endTime).toLocaleString("sv-SE")}
+          🗓 Start: {new Date(occurrence.startTime).toLocaleTimeString("sv-SE", { hour: '2-digit', minute: '2-digit' })}
         </p>
         <p className="occurrence-card-info">
-          📍 Plats-ID: {occurrence.subLocationId}
+          ⏰ Slut: {new Date(occurrence.endTime).toLocaleTimeString("sv-SE", { hour: '2-digit', minute: '2-digit' })}
+        </p>
+        <p className="occurrence-card-info">
+          📍 Plats: {occurrence.subLocationName}
         </p>
         <p className="occurrence-card-info">
           👥 {occurrence.availableSpots} / {occurrence.capacity} platser lediga
+        </p>
+        <p className="occurrence-card-info">
+          Pris: {occurrence.price}kr
         </p>
         {occurrence.isCancelled && (
           <p className="occurrence-card-cancelled">❌ Inställd</p>
