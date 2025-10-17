9747864fb580ebee009ab2387b67b06b84ee0208
darkdusk234 <johan.svensson.01@hotmail.com>
2025-10-17 11:43:14 +0200

    Created  registration form file with html form

---
+++ Client/src/Components/RegistrationForm.jsx
@@ -0,0 +1,70 @@
+import React, { useState } from 'react'
+import { useAuth } from '../contexts/AuthContext';
+
+const RegistrationForm = () => {
+    const [userName, setUserName] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [firstName, setFirstName] = useState("");
+    const [lastName, setLastName] = useState("");
+    const [birthDate, setBirthDate] = useState("");
+    const [adress, setAdress] = useState("");
+  return (
+    <>
+    <form onSubmit={handleSubmit}>
+            <input
+                type="text"
+                value={userName}
+                onChange={(e) => setUserName(e.target.value)}
+                placeholder="Username"
+                required
+            />
+            <input
+                type="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                placeholder="Password"
+                required
+            />
+            <input
+                type="text"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                placeholder="Email"
+                required
+            />
+            <input
+                type="text"
+                value={firstName}
+                onChange={(e) => setFirstName(e.target.value)}
+                placeholder="First Name"
+                required
+            />
+            <input
+                type="text"
+                value={lastName}
+                onChange={(e) => setLastName(e.target.value)}
+                placeholder="Last Name"
+                required
+            />
+            <input
+                type="date"
+                value={birthDate}
+                onChange={(e) => setBirthDate(e.target.value)}
+                placeholder="Date Of Birth"
+                required
+            />
+            <input
+                type="text"
+                value={adress}
+                onChange={(e) => setAdress(e.target.value)}
+                placeholder="Adress"
+                required
+            />
+            <button type="submit">Register</button>
+        </form>
+    </>
+  )
+}
+
+export default RegistrationForm
