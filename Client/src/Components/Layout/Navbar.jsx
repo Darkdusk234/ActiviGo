ed8827982bae7da20fde8920d20fd5d16b11fab3
Edgarskonnegard <j.edgarskonnegard@gmail.com>
2025-10-18 20:43:25 +0200

    Moved search method.

--- Client/src/Components/Layout/Navbar.jsx
+++ Client/src/Components/Layout/Navbar.jsx
@@ -4,11 +4,6 @@ import NavLogin from "./NavLogin";
 import './Layout.css'
 
 export default function Navbar() {
-  const handleSearch = (query) => {
-    console.log("Söker efter:", query);
-    // Här kan du navigera eller filtrera listor baserat på query
-  };
-
   return (
     <nav
 
@@ -17,13 +12,7 @@ export default function Navbar() {
         <Link to="/">Home</Link>
         <Link to="/booking">Booking</Link>
         <Link to="/about">About</Link>
-
         <Link to="/search">Search</Link>
-
-        
-      </div>
-      <div>
-        <SearchBar/>
       </div>
       <div>
       <NavLogin />
