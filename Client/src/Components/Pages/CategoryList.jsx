e35fb585049aaedb2981e647264120437f655d0f
darkdusk234 <johan.svensson.01@hotmail.com>
2025-10-20 20:28:04 +0200

    Created categoryList Page

---
+++ Client/src/Components/Pages/CategoryList.jsx
@@ -0,0 +1,14 @@
+import React, { useState } from 'react'
+import { useCategories } from '../../contexts/CategoryContext'
+
+const CategoryList = () => {
+    const { categories } = useCategories
+
+  return (
+    <>
+    
+    </>
+  )
+}
+
+export default CategoryList
