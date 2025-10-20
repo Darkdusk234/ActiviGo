a498a72a56ee48817ac6eae2f675a5a8951c720c
darkdusk234 <johan.svensson.01@hotmail.com>
2025-10-20 20:32:13 +0200

    Added list of categories to page

--- Client/src/Components/Pages/CategoryList.jsx
+++ Client/src/Components/Pages/CategoryList.jsx
@@ -1,12 +1,21 @@
 import React, { useState } from 'react'
 import { useCategories } from '../../contexts/CategoryContext'
+import CategoryCard from '../Cards/CategoryCard'
 
 const CategoryList = () => {
     const { categories } = useCategories
 
   return (
     <>
-    
+    <div className='CategoryListContainer'>
+        <ul className='CategoryList'>
+            {categories.map((category, index) => (
+                <li key={index}>
+                    <CategoryCard category={category} />
+                </li>
+            ))}
+        </ul>
+    </div>
     </>
   )
 }
