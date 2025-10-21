2e849a93544b2c7cccd4375f2b5fc7b04a541059
darkdusk234 <johan.svensson.01@hotmail.com>
2025-10-21 13:47:23 +0200

    Fixed the bug

--- Client/src/contexts/AuthContext.jsx
+++ Client/src/contexts/AuthContext.jsx
@@ -95,10 +95,7 @@ export const AuthProvider = ({ children }) => {
 
             return data;
 
-        } catch (error) {
-            console.error('Error logging in:', error);
-            throw error;
-        }
+        })
     };
 
     const logout = (user) => {
