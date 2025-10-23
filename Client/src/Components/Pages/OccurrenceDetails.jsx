ab72de7ab1b34cf37c77806c927efa3d74dd68ce
Edgarskonnegard <j.edgarskonnegard@gmail.com>
2025-10-23 10:12:24 +0200

    restyle booking page

--- Client/src/Components/Pages/OccurrenceDetails.jsx
+++ Client/src/Components/Pages/OccurrenceDetails.jsx
@@ -1,6 +1,8 @@
 import { useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
 import { useAuth } from '../../contexts/AuthContext';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faMapMarkerAlt, faCalendarAlt, faClock, faInfoCircle, faUsers, faTag, faMoneyBillWave } from '@fortawesome/free-solid-svg-icons';
 import "./OccurrenceDetails.css";
 
 const OccurrenceDetails = () => {
@@ -12,7 +14,6 @@ const OccurrenceDetails = () => {
   const [endTime, setEndTime] = useState('');
   const [participants, setParticipants] = useState('');
   const { user, loading: authLoading, APIURL, fetchUser } = useAuth();
-
 
   useEffect(() => {
     const fetchOccurrence = async () => {
@@ -40,19 +41,17 @@ const OccurrenceDetails = () => {
   const handleBooking = async (e) => {
     e.preventDefault();
     const currentUser = await fetchUser();
-  if (!currentUser) {
-    alert("Du måste vara inloggad för att boka!");
-    return;
-  }
+    if (!currentUser) {
+      alert("Du måste vara inloggad för att boka!");
+      return;
+    }
 
-    if (!participants || isNaN(participants) 
-      || participants <= 0 || participants <= occurrence.availableSpots) 
-    {
+    if (!participants || isNaN(participants) || participants <= 0 || participants <= occurrence.availableSpots) {
       try {
-        const res = await fetch(`${API_URL}/Booking`, {
+        const res = await fetch(`${APIURL}/Booking`, {
           method: "Post",
           headers: { "Content-Type": "application/json" },
-          body: JSON.stringify({"activityOccurenceId": id, "participants": participants})
+          body: JSON.stringify({ "activityOccurenceId": id, "participants": participants })
         });
 
         if (res.ok) {
@@ -65,48 +64,57 @@ const OccurrenceDetails = () => {
       }
     }
   };
+
   const splitDateTime = (startTime, endTime) => {
-        const [startDatePart, startTimePart] = startTime.split('T');
-        const [endDatePart, endTimePart] = endTime.split('T');
-        setDate(startDatePart);
-        setStartTime(startTimePart.slice(0,-3));
-        setEndTime(endTimePart.slice(0,-3));
-    }
+    const [startDatePart, startTimePart] = startTime.split('T');
+    const [endDatePart, endTimePart] = endTime.split('T');
+    setDate(startDatePart);
+    setStartTime(startTimePart.slice(0, -3));
+    setEndTime(endTimePart.slice(0, -3));
+  };
 
   if (loading) return <div>Laddar...</div>;
   if (!occurrence) return <div>Ingen aktivitet hittades</div>;
 
   return (
     <div className="occurrence-details-container">
-     {console.log(occurrence)}
-      <div className="details-title">
-        <h4>{occurrence.activityName}</h4>
-        <img src={`https://picsum.photos/400/250?random=${occurrence.id}`} alt={occurrence.activityName}/>
-      </div>
-        <h2>Location: {occurrence.subLocationName}, {occurrence.locationName} </h2>
-        <h2>date: {date}</h2>
-        <h2>time: {startTime} - {endTime}</h2>
-        <h2>Description: {occurrence.activityDescription}</h2>
-        <h2>available spots: {occurrence.availableSpots}/{occurrence.capacity}</h2>
-        <h2>Category: {occurrence.categoryName}</h2>
-        <h2>Price: {!occurrence.price ? 0 : occurrence.price} SEK</h2>
-            
-      <form onSubmit={handleBooking} className="booking-form">
-        <div>
-          <label>Participants</label>
-          <input
-            type="text"
-            value={participants}
-            onChange={(e) => setParticipants(e.target.value)}
-            required
-          />
+      <div className="img-container"></div>
+      <div className="details-title"></div>
+      <div className="text-container">
+        <div className="content-info" style={{ backgroundImage: `url(https://picsum.photos/400/250?random=${occurrence.id})` }}>
+          <h4>{occurrence.activityName}</h4>
+          <h2><FontAwesomeIcon icon={faMapMarkerAlt} /> Plats: {occurrence.subLocationName}, {occurrence.locationName}</h2>
+          <h2><FontAwesomeIcon icon={faCalendarAlt} /> Datum: {date}</h2>
+          <h2><FontAwesomeIcon icon={faClock} /> Tid: {startTime} - {endTime}</h2>
+          <h2><FontAwesomeIcon icon={faInfoCircle} /> Beskrivning: {occurrence.activityDescription}</h2>
+          <h2><FontAwesomeIcon icon={faUsers} /> Lediga platser: {occurrence.availableSpots}/{occurrence.capacity}</h2>
+          <h2><FontAwesomeIcon icon={faTag} /> Kategori: {occurrence.categoryName}</h2>
+          <h2><FontAwesomeIcon icon={faMoneyBillWave} /> Pris: {!occurrence.price ? 0 : occurrence.price} SEK</h2>
         </div>
-        <button
-          type="submit"
-        >
-          Boka
-        </button>
-      </form>
+        <div className="container-wrapper">
+
+          <div className="book-container">
+            <h3>Boka aktivitet</h3>
+            <p>Ange antalet platser du vill boka</p>
+            <div className="book-availablespots">
+              <h2>Lediga platser: {occurrence.availableSpots}</h2>
+
+            </div>
+            <form onSubmit={handleBooking} className="booking-form">
+              <div>
+                <label>Deltagare</label>
+                <input
+                  type="text"
+                  value={participants}
+                  onChange={(e) => setParticipants(e.target.value)}
+                  required
+                />
+              </div>
+              <button type="submit">Boka</button>
+            </form>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
