bbb0d88e96666724f7c8c39447de0092d0bc5052
Edgarskonnegard <j.edgarskonnegard@gmail.com>
2025-10-23 10:10:39 +0200

    Category card styling.

--- Client/src/Components/Cards/CategoryCard.jsx
+++ Client/src/Components/Cards/CategoryCard.jsx
@@ -1,8 +1,11 @@
 import "./CategoryCard.css";
 
 export default function CategoryCard({ category }) {
+  const imageUrl = `https://picsum.photos/400/250?random=${category.id}`;
   return (
-    <div className="category-card">
+    <div className="category-card"
+    style={{ backgroundImage: imageUrl ? `url(${imageUrl})` : 'none' }}
+    >
       <div className="category-card-content">
         <h3 className="category-card-title">{category.name}</h3>
         {category.description && (
